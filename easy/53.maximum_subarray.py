9b7cd611e6208a11d5b0cb052d0fda6c1e3f15b6
Yuri Liang <yuri.liang@ericsson.com>
2020-06-23 17:58:58 +0800

    add 53

--- easy/53.maximum_subarray.py
+++ easy/53.maximum_subarray.py
@@ -24,36 +24,55 @@
 # and conquer approach, which is more subtle.
 #
 # [End of Description]:
+# divide and conquer
+# class Solution:
+#     def maxSubArray(self, nums: List[int]) -> int:
+#         return self.helper(nums, 0, len(nums) - 1)
+
+#     def helper(self, nums: List[int], left: int, right: int) -> int:
+#         if left == right:
+#             return nums[left]
+
+#         p = (left + right) // 2
+
+#         left_sum = self.helper(nums, left, p)
+#         right_sum = self.helper(nums, p + 1, right)
+#         cross_sum = self.cross_sum(nums, left, right, p)
+
+#         return max(left_sum, right_sum, cross_sum)
+
+#     def cross_sum(self, nums: List[int], left: int, right: int, p: int) -> int:
+#         if left == right:
+#             return nums[left]
+
+#         left_subsum = float("-inf")
+#         cur_sum = 0
+#         for i in range(p, left - 1, -1):
+#             cur_sum += nums[i]
+#             left_subsum = max(cur_sum, left_subsum)
+
+#         right_subsum = float("-inf")
+#         cur_sum = 0
+#         for i in range(p + 1, right + 1):
+#             cur_sum += nums[i]
+#             right_subsum = max(cur_sum, right_subsum)
+
+#         return left_subsum + right_subsum
+# greedy
+# class Solution:
+#     def maxSubArray(self, nums: List[int]) -> int:
+#         cur_sum = max_sum = nums[0]
+
+#         for i in range(1, len(nums)):
+#             cur_sum = max(nums[i], cur_sum + nums[i])
+#             max_sum = max(max_sum, cur_sum)
+#         return max_sum
+# dynamic programming
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        return self.helper(nums, 0, len(nums) - 1)
-
-    def helper(self, nums: List[int], left: int, right: int) -> int:
-        if left == right:
-            return nums[left]
-
-        p = (left + right) // 2
-
-        left_sum = self.helper(nums, left, p)
-        right_sum = self.helper(nums, p + 1, right)
-        cross_sum = self.cross_sum(nums, left, right, p)
-
-        return max(left_sum, right_sum, cross_sum)
-
-    def cross_sum(self, nums: List[int], left: int, right: int, p: int) -> int:
-        if left == right:
-            return nums[left]
-
-        left_subsum = float("-inf")
-        cur_sum = 0
-        for i in range(p, left - 1, -1):
-            cur_sum += nums[i]
-            left_subsum = max(cur_sum, left_subsum)
-
-        right_subsum = float("-inf")
-        cur_sum = 0
-        for i in range(p + 1, right + 1):
-            cur_sum += nums[i]
-            right_subsum = max(cur_sum, right_subsum)
-
-        return left_subsum + right_subsum
+        max_sum = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i - 1] > 0:
+                nums[i] += nums[i - 1]
+            max_sum = max(nums[i], max_sum)
+        return max_sum
