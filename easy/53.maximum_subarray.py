3d02f5dbb5e257995750c23a0930eef09acfed40
Yuri <liangy3928@gmail.com>
2020-06-23 08:37:30 +0800

    53 maximum subarray

---
+++ easy/53.maximum_subarray.py
@@ -0,0 +1,59 @@
+# Maximum Subarray
+#
+# [Easy] [AC:46.2% 1M of 2.2M]
+# [filetype:python3]
+#
+# Given an integer array nums, find the
+# contiguous subarray (containing at least one
+# number) which has the largest sum and return
+# its sum.
+#
+# Example:
+#
+# Input: [-2,1,-3,4,-1,2,1,-5,4],
+#
+# Output: 6
+#
+# Explanation: [4,-1,2,1] has the largest sum
+# = 6.
+#
+# Follow up:
+#
+# If you have figured out the O(n) solution,
+# try coding another solution using the divide
+# and conquer approach, which is more subtle.
+#
+# [End of Description]:
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        return self.helper(nums, 0, len(nums) - 1)
+
+    def helper(self, nums: List[int], left: int, right: int) -> int:
+        if left == right:
+            return nums[left]
+
+        p = (left + right) // 2
+
+        left_sum = self.helper(nums, left, p)
+        right_sum = self.helper(nums, p + 1, right)
+        cross_sum = self.cross_sum(nums, left, right, p)
+
+        return max(left_sum, right_sum, cross_sum)
+
+    def cross_sum(self, nums: List[int], left: int, right: int, p: int) -> int:
+        if left == right:
+            return nums[left]
+
+        left_subsum = float("-inf")
+        cur_sum = 0
+        for i in range(p, left - 1, -1):
+            cur_sum += nums[i]
+            left_subsum = max(cur_sum, left_subsum)
+
+        right_subsum = float("-inf")
+        cur_sum = 0
+        for i in range(p + 1, right + 1):
+            cur_sum += nums[i]
+            right_subsum = max(cur_sum, right_subsum)
+
+        return left_subsum + right_subsum
