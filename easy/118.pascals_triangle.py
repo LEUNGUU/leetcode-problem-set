933ae1e4a3124e8b2d3635406646545597bbde6e
Yuri Liang <yuri.liang@ericsson.com>
2020-06-28 17:37:44 +0800

    add more problems

--- easy/118.pascals_triangle.py
+++ easy/118.pascals_triangle.py
@@ -1,43 +1,42 @@
 # Pascal's Triangle
-# 
+#
 # [Easy] [AC:51.7% 373K of 720.9K] [filetype:python3]
-# 
+#
 # Given a non-negative integer numRows, generate the first numRows of
 # Pascal's triangle.
-# 
+#
 # In Pascal's triangle, each number is the sum of the two numbers
 # directly above it.
-# 
+#
 # Example:
-# 
+#
 # Input: 5
-# 
+#
 # Output:
-# 
+#
 # [
-# 
+#
 #      [1],
-# 
+#
 #     [1,1],
-# 
+#
 #    [1,2,1],
-# 
+#
 #   [1,3,3,1],
-# 
+#
 #  [1,4,6,4,1]
-# 
+#
 # ]
-# 
+#
 # [End of Description]:
 # Dynamic Programming
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         triangle = []
         for row_num in range(numRows):
-            row = [None for _ in range(row_num+1)]
+            row = [None for _ in range(row_num + 1)]
             row[0], row[-1] = 1, 1
             for j in range(1, len(row) - 1):
-                row[j] = triangle[row_num-1][j-1] + triangle[row_num-1][j]
+                row[j] = triangle[row_num - 1][j - 1] + triangle[row_num - 1][j]
             triangle.append(row)
         return triangle
-        
