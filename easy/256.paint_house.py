75f73bf98c51d6e8cb265ca1b5fafe7c93f62ac2
Yuri <liangy3928@gmail.com>
2020-07-14 07:32:40 +0800

    add more problems

---
+++ easy/256.paint_house.py
@@ -0,0 +1,45 @@
+# Paint House
+#
+# [Easy] [AC:52.0% 86.7K of 166.8K] [filetype:python3]
+#
+# There are a row of n houses, each house can be painted with one of
+# the three colors: red, blue or green. The cost of painting each
+# house with a certain color is different. You have to paint all the
+# houses such that no two adjacent houses have the same color.
+#
+# The cost of painting each house with a certain color is represented
+# by a n x 3 cost matrix. For example, costs[0][0] is the cost of
+# painting house 0 with color red; costs[1][2] is the cost of painting
+# house 1 with color green, and so on... Find the minimum cost to
+# paint all houses.
+#
+# Note:
+#
+# All costs are positive integers.
+#
+# Example:
+#
+# Input: [[17,2,17],[16,16,5],[14,3,19]]
+#
+# Output: 10
+#
+# Explanation: Paint house 0 into blue, paint house 1 into green,
+# paint house 2 into blue.
+#
+#              Minimum cost: 2 + 5 + 3 = 10.
+#
+# [End of Description]:
+class Solution:
+    def minCost(self, costs: List[List[int]]) -> int:
+        zero_cost = (0, -1)
+        minimum_cost = 0
+        for item in costs:
+            temp_cost = float("inf")
+            for index, cost in enumerate(item):
+                if cost < temp_cost and index != zero_cost[1]:
+                    temp_cost = cost
+                    next_cost = (temp_cost, index)
+            zero_cost = next_cost
+            print(zero_cost)
+            minimum_cost += next_cost[0]
+        return minimum_cost
