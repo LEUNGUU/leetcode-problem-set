be61c55e581e8a13bf3ee1748caf43e06f82d19b
Yuri <liangy3928@gmail.com>
2020-07-29 08:02:48 +0800

    add more problems

--- easy/DynamicProgramming/256.paint_house.py
+++ easy/DynamicProgramming/256.paint_house.py
@@ -29,29 +29,55 @@
 #              Minimum cost: 2 + 5 + 3 = 10.
 #
 # [End of Description]:
-from functools import lru_cache
+# from functools import lru_cache
+
+
+# class Solution:
+#     def minCost(self, costs: List[List[int]]) -> int:
+#         """
+#         :type costs: List[List[int]]
+#         :rtype: int
+#         """
+
+#         @lru_cache(maxsize=None)
+#         def paint_cost(n, color):
+#             total_cost = costs[n][color]
+#             if n == len(costs) - 1:
+#                 pass
+#             elif color == 0:
+#                 total_cost += min(paint_cost(n + 1, 1), paint_cost(n + 1, 2))
+#             elif color == 1:
+#                 total_cost += min(paint_cost(n + 1, 0), paint_cost(n + 1, 2))
+#             else:
+#                 total_cost += min(paint_cost(n + 1, 0), paint_cost(n + 1, 1))
+#             return total_cost
+
+#         if costs == []:
+#             return 0
+#         return min(paint_cost(0, 0), paint_cost(0, 1), paint_cost(0, 2))
+
+# class Solution:
+#     def minCost(self, costs: List[List[int]]) -> int:
+#         for n in reversed(range(len(costs) - 1)):
+#             costs[n][0] += min(costs[n + 1][1], costs[n + 1][2])
+#             costs[n][1] += min(costs[n + 1][0], costs[n + 1][2])
+#             costs[n][2] += min(costs[n + 1][0], costs[n + 1][1])
+#         if len(costs) == 0:
+#             return 0
+#         return min(costs[0])
+
+from copy import deepcopy
 
 
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
-        """
-        :type costs: List[List[int]]
-        :rtype: int
-        """
-
-        @lru_cache(maxsize=None)
-        def paint_cost(n, color):
-            total_cost = costs[n][color]
-            if n == len(costs) - 1:
-                pass
-            elif color == 0:
-                total_cost += min(paint_cost(n + 1, 1), paint_cost(n + 1, 2))
-            elif color == 1:
-                total_cost += min(paint_cost(n + 1, 0), paint_cost(n + 1, 2))
-            else:
-                total_cost += min(paint_cost(n + 1, 0), paint_cost(n + 1, 1))
-            return total_cost
-
-        if costs == []:
+        if len(costs) == 0:
             return 0
-        return min(paint_cost(0, 0), paint_cost(0, 1), paint_cost(0, 2))
+        previous_row = costs[-1]
+        for n in reversed(range(len(costs) - 1)):
+            current_row = deepcopy(costs[n])
+            current_row[0] += min(previous_row[1], previous_row[2])
+            current_row[1] += min(previous_row[0], previous_row[2])
+            current_row[2] += min(previous_row[0], previous_row[1])
+            previous_row = current_row
+        return min(previous_row)
