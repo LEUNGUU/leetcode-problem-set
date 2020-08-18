fa9d106825f44dc93787835e022e5d2d61b154dc
Yuri <liangy3928@gmail.com>
2020-08-18 08:44:15 +0800

    add more problems

---
+++ easy/HashTable/290.word_pattern.py
@@ -0,0 +1,63 @@
+# Word Pattern
+#
+# [Easy] [AC:37.0% 193.8K of 523.8K] [filetype:python3]
+#
+# Given a pattern and a string str, find if str follows the same pattern.
+#
+# Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in
+# str.
+#
+# Example 1:
+#
+# Input: pattern = "abba", str = "dog cat cat dog"
+#
+# Output: true
+#
+# Example 2:
+#
+# Input:pattern = "abba", str = "dog cat cat fish"
+#
+# Output: false
+#
+# Example 3:
+#
+# Input: pattern = "aaaa", str = "dog cat cat dog"
+#
+# Output: false
+#
+# Example 4:
+#
+# Input: pattern = "abba", str = "dog dog dog dog"
+#
+# Output: false
+#
+# Notes:
+#
+# You may assume pattern contains only lowercase letters, and str contains lowercase letters that may be separated by
+# a single space.
+#
+# [End of Description]:
+class Solution:
+    def wordPattern(self, pattern: str, str: str) -> bool:
+        map_index = {}
+        words = str.split()
+
+        if len(pattern) != len(words):
+            return False
+
+        for index in range(len(words)):
+            p = pattern[index]
+            w = words[index]
+
+            p_key = "pattern_{}".format(p)
+            w_key = "word_{}".format(w)
+
+            if p_key not in map_index:
+                map_index[p_key] = index
+
+            if w_key not in map_index:
+                map_index[w_key] = index
+
+            if map_index[p_key] != map_index[w_key]:
+                return False
+        return True
--- easy/TwoPointer/283.move_zeroes.py
+++ easy/TwoPointer/283.move_zeroes.py
@@ -1,23 +1,3 @@
-# Move Zeroes
-#
-# [Easy] [AC:57.8% 877K of 1.5M] [filetype:python3]
-#
-# Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the
-# non-zero elements.
-#
-# Example:
-#
-# Input: [0,1,0,3,12]
-#
-# Output: [1,3,12,0,0]
-#
-# Note:
-#
-# You must do this in-place without making a copy of the array.
-#
-# Minimize the total number of operations.
-#
-# [End of Description]:
 # class Solution:
 #     def moveZeroes(self, nums: List[int]) -> None:
 #         """
@@ -36,9 +16,8 @@ class Solution:
             if nums[pa] == 0 and nums[pb] != 0:
                 nums[pa], nums[pb] = nums[pb], nums[pa]
                 pa += 1
-                pb += 1
             elif nums[pa] == 0 and nums[pb] == 0:
-                pb += 1
+                pass
             else:
                 pa += 1
-                pb += 1
+            pb += 1
