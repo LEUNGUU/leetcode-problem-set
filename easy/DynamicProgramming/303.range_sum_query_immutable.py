f9c4a0f0d94da876f047a3cede6f23e8ff740bc8
Yuri <liangy3928@gmail.com>
2020-08-20 08:43:47 +0800

    add more problems

---
+++ easy/DynamicProgramming/303.range_sum_query_immutable.py
@@ -0,0 +1,52 @@
+# Range Sum Query - Immutable
+#
+# [Easy] [AC:44.9% 209.2K of 466.1K] [filetype:python3]
+#
+# Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+#
+# Example:
+#
+# Given nums = [-2, 0, 3, -5, 2, -1]
+#
+# sumRange(0, 2) -> 1
+#
+# sumRange(2, 5) -> -1
+#
+# sumRange(0, 5) -> -3
+#
+# Constraints:
+#
+# You may assume that the array does not change.
+#
+# There are many calls to sumRange function.
+#
+# 0 <= nums.length <= 10^4
+#
+# -10^5 <= nums[i] <= 10^5
+#
+# 0 <= i <= j < nums.length
+#
+# [End of Description]:
+
+# class NumArray:
+#
+#     def __init__(self, nums: List[int]):
+#         self.nums = nums
+#
+#     def sumRange(self, i: int, j: int) -> int:
+#         # return sum([self.nums[idx] for idx in range(i, j+1)])
+#         return sum(self.nums[i:j+1])
+# Caching
+class NumArray:
+    def __init__(self, nums: List[int]):
+        self.res = [0] * (len(nums) + 1)
+        for idx in range(len(nums)):
+            self.res[idx + 1] = self.res[idx] + nums[idx]
+
+    def sumRange(self, i: int, j: int) -> int:
+        return self.res[j + 1] - self.res[i]
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(i,j)
---
+++ easy/Math/326.power_of_three.py
@@ -0,0 +1,49 @@
+# Power of Three
+#
+# [Easy] [AC:42.1% 272.3K of 647.3K] [filetype:python3]
+#
+# Given an integer, write a function to determine if it is a power of three.
+#
+# Example 1:
+#
+# Input: 27
+#
+# Output: true
+#
+# Example 2:
+#
+# Input: 0
+#
+# Output: false
+#
+# Example 3:
+#
+# Input: 9
+#
+# Output: true
+#
+# Example 4:
+#
+# Input: 45
+#
+# Output: false
+#
+# Follow up:
+#
+# Could you do it without using any loop / recursion?
+#
+# [End of Description]:
+
+# class Solution:
+#     def isPowerOfThree(self, n: int) -> bool:
+#         if n == 0:
+#             return False
+#         while (n % 3) == 0:
+#             n /= 3
+#         return n == 1
+
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        bigger = math.pow(3, 20)
+        return n > 0 and bigger % n == 0
