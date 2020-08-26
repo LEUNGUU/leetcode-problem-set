937e0cc9ce7f3056ddb662845c0d9bc0ef4d7ceb
Yuri Liang <yuri.liang@ericsson.com>
2020-08-26 18:04:42 +0800

    reformat files

--- easy/BitManipulation/136.single_number.py
+++ easy/BitManipulation/136.single_number.py
@@ -23,26 +23,44 @@
 # Output: 4
 #
 # [End of Description]:
+
+# list operation
+# Iterate over all the elements in nums
+# If some number in nums is new to array, append it
+# If some number is already in the array, remove it
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        tmp = []
+        for item in nums:
+            if item in tmp:
+                tmp.remove(item)
+            else:
+                tmp.append(item)
+        return tmp.pop()
+
+
 # hash table to record elements
-# class Solution:
-#     def singleNumber(self, nums: List[int]) -> int:
-#         res = {}
-#         for i in range(len(nums)):
-#             if nums[i] not in res:
-#                 res[nums[i]] = 1
-#             else:
-#                 res[nums[i]] += 1
-#         for k, v in res.items():
-#             if v == 1:
-#                 return k
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = {}
+        for i in range(len(nums)):
+            if nums[i] not in res:
+                res[nums[i]] = 1
+            else:
+                res[nums[i]] += 1
+        for k, v in res.items():
+            if v == 1:
+                return k
 
-# mathmatical computation
+
+# Mathmatical computation
 # [a, a, c, b, c]: 2*(a+b+c) - (a+a+c+b+c) = b
-# class Solution:
-#     def singleNumber(self, nums: List[int]) -> int:
-#         return 2*sum(set(nums)) - sum(nums)
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        return 2*sum(set(nums)) - sum(nums)
 
-# bit manipulation
+
+# Bit Manipulation
 # use XOR. a ^ a = 0 and a ^ 0 = a
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
