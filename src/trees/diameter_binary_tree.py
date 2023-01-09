28f2a4c0537068e7e5487f2ae7fcb9baeac6cc2c
leunguu <liangy3928@gmail.com>
2023-01-09 21:53:19 +0800

    add balanced tree

--- src/trees/diameter_binary_tree.py
+++ src/trees/diameter_binary_tree.py
@@ -1,10 +1,12 @@
 from typing import Optional
 
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
 
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
@@ -20,5 +22,6 @@ class Solution:
             res = max(res, 2 + left + right)
 
             return 1 + max(left, right)
+
         dfs(root)
         return res
