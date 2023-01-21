314d748e5ff70bd58f45eabce79ae77fa0d6f939
leunguu <liangy3928@gmail.com>
2023-01-21 09:57:36 +0800

    add two numbers

---
+++ src/linked_list/add_two_numbers.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def addTwoNumbers(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        dummy = ListNode()
+        curr = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+
+            value = v1 + v2 + carry
+            carry = value // 10
+            val = value % 10
+
+            curr.next = ListNode(val)
+            curr = curr.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return dummy.next
