ebba44fefd5013d5a61cd4d3ef710f2c07361f7c
leunguu <liangy3928@gmail.com>
2023-02-09 09:01:55 +0800

    add n queens

---
+++ src/backtracking/n_queens.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        cols = set()
+        posDiag = set()
+        negDiag = set()
+
+        res: List[List[str]] = []
+        board = [["."] * n for _ in range(n)]
+
+        def backtrack(r: int) -> None:
+            if r == n:
+                copy = ["".join(row) for row in board]
+                res.append(copy)
+                return
+
+            for c in range(n):
+                if c in cols or (r + c) in posDiag or (r - c) in negDiag:
+                    continue
+                cols.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+                board[r][c] = "Q"
+                backtrack(r + 1)
+                cols.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
+                board[r][c] = "."
+
+        backtrack(0)
+        return res
