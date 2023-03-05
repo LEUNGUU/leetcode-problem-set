46b6dba9156aa9cb4e443a4edf7bfd2560b50d4d
leunguu <liangy3928@gmail.com>
2023-03-05 09:27:47 +0800

    add coin change

---
+++ src/1_D_dynamic_programming/coin_change.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+
+        for a in range(1, amount + 1):
+            for c in coins:
+                if (rest := a - c) >= 0:
+                    dp[a] = min(dp[a], 1 + dp[rest])
+        return dp[amount] if dp[amount] != amount + 1 else -1
