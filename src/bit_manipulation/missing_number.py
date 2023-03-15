37804daef056e56f251177c0f705291f53f3af8c
leunguu <liangy3928@gmail.com>
2023-03-15 12:20:31 +0800

    add missing number

---
+++ src/bit_manipulation/missing_number.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        res = len(nums)
+
+        for i in range(len(nums)):
+            res += i - nums[i]
+        return res
